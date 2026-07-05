0b2150159723f9d8a3885c953d78effe8479f182
Samradh Chaudhary <samradhchaudhary77@gmail.com>
2026-07-05 13:56:46 +0530

    Create BTmaxpathSum.java

---
+++ Week4/Day4/BTmaxpathSum.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    private int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, maxGain(node.left));
+        int right = Math.max(0, maxGain(node.right));
+
+        maxSum = Math.max(maxSum, node.val + left + right);
+
+        return node.val + Math.max(left, right);
+    }
+}T
